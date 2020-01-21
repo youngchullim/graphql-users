7bd640523ff70a9864bcb3c38306ca01e4ba64a2
Young Chul Lim <youngchullim95@gmail.com>
2020-01-20 20:51:14 -0800

    Add companies to our RootQueryType

--- schema/schema.js
+++ schema/schema.js
@@ -40,7 +40,15 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
         return axios.get(`http://localhost:3000/users/${args.id}`)
-          .then( resp => resp.data);
+          .then( res => res.data);
+      }
+    },
+    company: {
+      type: CompanyType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return axios.get(`http://localhost:3000/companies/${args.id}`)
+          .then( res => res.data);
       }
     }
   } 
